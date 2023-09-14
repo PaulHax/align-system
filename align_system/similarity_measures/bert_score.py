2dd14713c69790c91467df73a0ef7918f0c19c73
David Joy <10147749+dmjoy@users.noreply.github.com>
2023-09-14 12:18:09 -0400

    Add other bert_score measures for forced mapping

--- align_system/similarity_measures/bert_score.py
+++ align_system/similarity_measures/bert_score.py
@@ -11,3 +11,19 @@ def bert_score_similarity_f1(str1, str2):
     p, r, f1 = score([str1], [str2], lang='en')
 
     return f1
+
+
+def bert_score_similarity_recall(str1, str2):
+    # See: https://github.com/Tiiiger/bert_score/blob/master/example/Demo.ipynb
+
+    p, r, f1 = score([str1], [str2], lang='en')
+
+    return r
+
+
+def bert_score_similarity_precision(str1, str2):
+    # See: https://github.com/Tiiiger/bert_score/blob/master/example/Demo.ipynb
+
+    p, r, f1 = score([str1], [str2], lang='en')
+
+    return p
