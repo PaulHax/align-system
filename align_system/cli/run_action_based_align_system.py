0d7c58445a827b8de2694b54c8b68dd0ebf895b1
David Joy <10147749+dmjoy@users.noreply.github.com>
2023-10-20 16:21:11 -0400

    Update action based system to use logging

--- align_system/cli/run_action_based_align_system.py
+++ align_system/cli/run_action_based_align_system.py
@@ -1,4 +1,9 @@
+import sys
 import json
+import logging
+
+from rich.logging import RichHandler
+from rich.highlighter import JSONHighlighter
 
 from align_system.interfaces.cli_builder import build_interfaces
 from align_system.algorithms.llm_baseline import LLMBaseline
@@ -8,6 +13,17 @@ from align_system.prompt_engineering.common import (
     prepare_action_based_prompt,
     prepare_treatment_selection_prompt,
     prepare_tagging_selection_prompt)
+
+
+LOGGING_FORMAT = "%(message)s"
+logging.basicConfig(
+    level="NOTSET",
+    format=LOGGING_FORMAT,
+    datefmt="[%X]",
+    handlers=[RichHandler()])
+JSON_HIGHLIGHTER = JSONHighlighter()
+
+log = logging.getLogger(__name__)
 
 
 def add_cli_args(parser):
@@ -35,6 +51,8 @@ def add_cli_args(parser):
 
 
 def main():
+    log.debug(f"[bright_black]CMD: {' '.join(sys.argv)}[/bright_black]",
+              extra={'markup': True, 'highlighter': None})
     run_action_based_align_system(
         **build_interfaces(add_cli_args, "ALIGN Action Based System CLI",
                            supported_interfaces={'TA3ActionBased'}))
@@ -103,13 +121,13 @@ def run_action_based_align_system(interface,
             for kdma_dict in alignment_target_dict.get('kdma_values', ()):
                 if kdma_dict['kdma'].lower() == 'knowledge':
                     if kdma_dict['value'] > 1:
-                        print("** Setting 'retrieval_enabled' to True based "
-                              "on 'Knowledge' KDMA value ({})".format(
-                                  kdma_dict['value']))
+                        log.info("** Setting 'retrieval_enabled' to True "
+                                 "based on 'Knowledge' KDMA value ({})".format(
+                                     kdma_dict['value']))
                         algorithm_kwargs_parsed['retrieval_enabled'] = True
                     else:
-                        print("** Setting 'retrieval_enabled' to False based "
-                              "on 'Knowledge' KDMA value ({})".format(
+                        log.info("** Setting 'retrieval_enabled' to False "
+                                 "based on 'Knowledge' KDMA value ({})".format(
                                   kdma_dict['value']))
                         algorithm_kwargs_parsed['retrieval_enabled'] = False
 
@@ -146,15 +164,21 @@ def run_action_based_align_system(interface,
             available_actions_unstructured,
             alignment_target=alignment_target_dict if align_to_target else None
         )
-        print("* Prompt for ADM: {}".format(prompt))
+        log.info("[bold]* Action prompt for ADM *[/bold]",
+                 extra={"markup": True})
+        log.info(prompt)
 
         raw_response = str(algorithm.run_inference(prompt))
-        print("* ADM raw response: {}".format(raw_response))
+        log.info("[bold]* ADM raw response *[/bold]",
+                 extra={"markup": True})
+        log.info(raw_response)
 
         selected_action_idx, selected_action = force_choice_func(
             raw_response, available_actions_unstructured)
 
-        print("* Mapped selection: '{}'".format(selected_action))
+        log.info("[bold]* Mapped selection *[/bold]",
+                 extra={"markup": True})
+        log.info(selected_action)
 
         action_to_take = available_actions[selected_action_idx]
 
@@ -174,13 +198,16 @@ def run_action_based_align_system(interface,
                 casualty_to_treat['vitals'],
                 current_state['supplies'])
 
-            print("** Treatment prompt for ADM: {}".format(treatment_prompt))
+            log.info("[bold]** Treatment prompt for ADM **[/bold]",
+                     extra={"markup": True})
+            log.info(treatment_prompt)
 
             raw_treatment_response =\
                 str(algorithm.run_inference(treatment_prompt))
 
-            print("** ADM raw treatment response: {}".format(
-                raw_treatment_response))
+            log.info("[bold]** ADM raw treatment response **[/bold]",
+                     extra={"markup": True})
+            log.info(raw_treatment_response)
 
             # Map response to treatment and treatment location
             _, treatment = force_choice_func(
@@ -191,8 +218,9 @@ def run_action_based_align_system(interface,
                 raw_treatment_response,
                 TREATMENT_LOCATIONS)
 
-            print("** Mapped treatment selection: '{}: {}'".format(
-                treatment, treatment_location))
+            log.info("[bold]** Mapped treatment selection **[/bold]",
+                     extra={"markup": True})
+            log.info("{}: {}".format(treatment, treatment_location))
 
             # Populate required parameters for treatment action
             action_to_take['parameters'] = {
@@ -205,13 +233,16 @@ def run_action_based_align_system(interface,
                 untagged_casualties,
                 CASUALTY_TAGS)
 
-            print("** Tagging prompt for ADM: {}".format(tagging_prompt))
+            log.info("[bold]** Tagging prompt for ADM **[/bold]",
+                     extra={"markup": True})
+            log.info(tagging_prompt)
 
             raw_tagging_response =\
                 str(algorithm.run_inference(tagging_prompt))
 
-            print("** ADM raw tagging response: {}".format(
-                raw_tagging_response))
+            log.info("[bold]** ADM raw tagging response **[/bold]",
+                     extra={"markup": True})
+            log.info(raw_tagging_response)
 
             # Map response to casualty to tag
             casualty_to_tag_idx, _ = force_choice_func(
@@ -225,12 +256,18 @@ def run_action_based_align_system(interface,
                 raw_tagging_response,
                 CASUALTY_TAGS)
 
-            print("** Mapped tag selection: '{}: {}'".format(
-                casualty_to_tag_id, tag))
+            log.info("[bold]** Mapped tag selection **[/bold]",
+                     extra={"markup": True})
+            log.info("{}: {}".format(casualty_to_tag_id, tag))
 
             # Populate required parameters for treatment action
             action_to_take['casualty_id'] = casualty_to_tag_id
             action_to_take['parameters'] = {'category': tag}
+
+        log.debug("[bold]*ACTION BEING TAKEN*[/bold]",
+                  extra={"markup": True})
+        log.debug(json.dumps(action_to_take, indent=4),
+                  extra={"highlighter": JSON_HIGHLIGHTER})
 
         current_state = scenario.take_action(action_to_take)
 
