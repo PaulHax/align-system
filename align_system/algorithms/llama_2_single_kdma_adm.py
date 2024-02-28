a6f7f200bd646bc7bcc4c3aba0473843bfc2549d
David Joy <10147749+dmjoy@users.noreply.github.com>
2024-02-28 16:30:24 -0500

    WIP add treatment and tagging selection functions to single kdma adm

--- align_system/algorithms/llama_2_single_kdma_adm.py
+++ align_system/algorithms/llama_2_single_kdma_adm.py
@@ -861,3 +861,127 @@ class Llama2SingleKDMAADM(AlignedDecisionMaker):
         }, target_kdma_values, labels=[target_kdma_values]*len(choices))
 
         return available_actions[response['choice']]
+
+    def populate_treatment_parameters(self, scenario_state, treatment_action, alignment_target, **kwargs):
+        from align_system.prompt_engineering.common import (
+            prepare_treatment_selection_prompt)
+        from swagger_client.models import ActionTypeEnum
+
+        assert treatment_action.action_type == ActionTypeEnum.APPLY_TREATMENT
+
+        character_id = treatment_action.character_id
+        matching_characters = [c for c in scenario_state.characters
+                               if c.id == character_id]
+
+        assert len(matching_characters) == 1
+        character_to_treat = matching_characters[0]
+
+        treatment_prompt = prepare_treatment_selection_prompt(
+            character_to_treat.unstructured,
+            character_to_treat.vitals.to_dict(),
+            [s.to_dict() for s in scenario_state.supplies])
+
+        for _ in range(kwargs.get('answer_attempts', 5)):
+            treatment_dialog =\
+                self.llm_algorithm.build_multiple_choice_dialog(
+                    treatment_prompt,
+                    [s.type for s in scenario_state.supplies],
+                    json_format=TREATMENT_MULTIPLE_CHOICE_JSON_FORMAT)
+
+            log.debug("[bold]*TREATMENT DIALOG*[/bold]",
+                      extra={"markup": True})
+            self.llm_algorithm.log_dialog(treatment_dialog)
+
+            raw_treatment_response = self.llm_algorithm.respond_to_dialog(
+                treatment_dialog)
+
+            log.info("** ADM raw treatment response: {}".format(
+                raw_treatment_response))
+
+            parsed_treatment_output = self.attempt_generic_parse(  # noqa
+                raw_treatment_response, ['Reasoning', 'Answer', 'Location'])  # noqa
+
+            if parsed_treatment_output is not None:
+                treatment_idx = parsed_treatment_output['Answer']
+
+                if len(scenario_state.supplies) <= treatment_idx:
+                    log.info('** Selected treatment_idx out of range of '
+                             'available treatment options, retrying!')
+                    continue
+
+                treatment = scenario_state.supplies[treatment_idx].type  # noqa
+
+                treatment_location = parsed_treatment_output['Location']
+
+                if treatment_action.parameters is None:
+                    treatment_action.parameters = {}
+
+                treatment_action.parameters['treatment'] = treatment
+                treatment_action.parameters['location'] = treatment_location
+
+                break
+            else:
+                log.info('** Failed to parse treatment')
+
+        return treatment_action
+
+    def populate_tagging_parameters(self, scenario_state, tagging_action, alignment_target, **kwargs):
+        from align_system.prompt_engineering.common import (
+            prepare_tagging_selection_prompt)
+        from swagger_client.models import ActionTypeEnum, CharacterTagEnum
+        from align_system.utils import get_swagger_class_enum_values
+
+        assert tagging_action.action_type == ActionTypeEnum.TAG_CHARACTER
+        # Ask the system to specify which triage tag to apply
+
+        untagged_characters = [c for c in scenario_state.characters if c.tag is None]
+
+        tagging_prompt = prepare_tagging_selection_prompt(
+            untagged_characters,
+            get_swagger_class_enum_values(CharacterTagEnum))
+
+        for _ in range(kwargs.get('answer_attempts', 5)):
+            tagging_dialog = self.build_multiple_choice_dialog(
+                tagging_prompt,
+                [c['unstructured'].strip()
+                 for c in untagged_characters],
+                json_format=TAGGING_MULTIPLE_CHOICE_JSON_FORMAT)
+
+            log.debug("[bold]*TAGGING DIALOG*[/bold]",
+                      extra={"markup": True})
+            self.log_dialog(tagging_dialog)
+
+            raw_tagging_response, _ = self.respond_to_dialog(
+                tagging_dialog)
+
+            log.info("** ADM raw tagging response: {}".format(
+                raw_tagging_response))
+
+            parsed_tagging_output = self.attempt_generic_parse(  # noqa
+                raw_tagging_response, ['Reasoning', 'Answer', 'Tag'])  # noqa
+
+            if parsed_tagging_output is not None:
+                character_idx = parsed_tagging_output['Answer']
+
+                if len(untagged_characters) <= character_idx:
+                    log.info('** Selected character_idx out of range of '
+                             'available treatment options, retrying!')
+                    continue
+
+                character_to_tag_id = untagged_characters[character_idx]['id']  # noqa
+
+                tag = parsed_tagging_output['Tag']
+
+                # Populate required parameters for tagging action
+                tagging_action.character_id = character_to_tag_id
+
+                if tagging_action.parameters is None:
+                    tagging_action.parameters = {}
+
+                tagging_action.parameters['category'] = tag
+
+                break
+            else:
+                log.info('** Failed to parse tagging')
+
+        return tagging_action
