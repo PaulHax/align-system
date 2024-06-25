e1bf5a16767ca0c2ee0565c534ec6df1487f944d
David Joy <10147749+dmjoy@users.noreply.github.com>
2024-06-25 12:44:31 -0400

    Remove old CLI scripts and supporting code

--- align_system/utils/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ProbeType(Enum):
-    MultipleChoice = "MultipleChoice"
-    FreeResponse = "FreeResponse"
-    PatientOrdering = "PatientOrdering"
-    SelectTag = "SelectTag"
-    SelectTreatment = "SelectTreatment"
