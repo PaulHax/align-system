88a3edf35f1775638110b66829f4e229d4999a5f
Jadie <jadieraeadams@gmail.com>
2024-07-24 14:56:45 -0400

    Adding ingroup bias COT

--- align_system/algorithms/outlines_regression_adm.py
+++ align_system/algorithms/outlines_regression_adm.py
@@ -115,8 +115,11 @@ class OutlinesTransformersRegressionADM(OutlinesTransformersADM):
         # Predicted scores are 0-10, KDMA values are 0-1
         expected_value = int(label * 10)
         cot_reasoning = ""
+
         # Moral derivngess or moral judgement
-        if target_kdma['kdma'] == 'MoralDesert' or 'Moral judgement':
+        # Example: Because Alex acted with no intent and did not cause of harm, 
+        #          choosing Treat Alex would score a 5 for the the principle of moral judgement.
+        if target_kdma['kdma'] == 'MoralDesert' or  target_kdma['kdma'] == 'Moral judgement':
             if hasattr(action, 'character_id') and action.character_id is not None:
                 # get character associated with action
                 action_character = None
@@ -137,6 +140,31 @@ class OutlinesTransformersRegressionADM(OutlinesTransformersADM):
                         cot_reasoning += f" and had a {action_character.directness_of_causality} cause of harm, choosing "
                 else:
                     raise RuntimeError(f"Character not found in state: {action.character_id}")
+        
+        # Ingroup bias
+        # Example: Patient A is a soldier in your unit and is in group Allied US, 
+        #           choosing to treat patient A would score a 0.6 for ingroup bias.
+        elif target_kdma['kdma'] == 'Ingroup Bias':
+            if hasattr(action, 'character_id') and action.character_id is not None:
+                # get character associated with action
+                action_character = None
+                for character in state.characters:
+                    if character.id == action.character_id:
+                        action_character = character
+                        break
+                if action_character is not None:
+                    # add unstructured charcter description to reasoning
+                    if hasattr(action_character, 'unstructured') and action_character.unstrucutured in not None:
+                        cot_reasoning += f"Because {action_character.name} is {lower(action_character.unstructured)}"
+                        # add military disposition
+                        if hasattr(action_character.demographics, 'military_disposition') and action_character.demographics.military_disposition is not None:
+                            cot_reasoning += f" and is in group {action_character.demographics.military_disposition}, choosing "
+                else:
+                    raise RuntimeError(f"Character not found in state: {action.character_id}")
+        
+        # TODO - Value of life
+        # TODO - Quality of life
+        
         else:
             cot_reasoning = f"Choosing "
         cot_reasoning += f"{choice} would score a {expected_value} for the the principle of {target_kdma['name']}."
